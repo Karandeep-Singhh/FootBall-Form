8409178b3313a0a913a925e6fe4bdb8b02ef2aa2
Karan <67946298+Karandeep-Singhh@users.noreply.github.com>
2022-02-14 19:12:00 +0530

    Update Validation.java

--- src/main/java/servlet/Validation.java
+++ src/main/java/servlet/Validation.java
@@ -4,9 +4,10 @@ import java.util.regex.Pattern;
 
 public class Validation {
 
-    public boolean isValid(Player player) {
+    public static boolean isValid(Player player) {
 //        boolean error = true;
 
+        System.out.println("validations start");
         if (!Pattern.matches("(\\w+)", player.getUserName())) return false;
         if(!Pattern.matches("^([a-zA-Z]{1,})$", player.getFirstName())) return false;
         if(player.getLastName().length() > 0 && !(Pattern.matches("[a-zA-Z\\s]+",player.getLastName()))) return false;
@@ -17,7 +18,16 @@ public class Validation {
         if(Integer.parseInt(player.getTeam()) < 1) return false;
         if(Integer.parseInt(player.getPosition()) < 1) return false;
         if(Pattern.matches("[@/=<>$]",player.getAddress())) return false;
-        if(!(Pattern.matches("[0-9]")))
+
+        if(player.getPinCode().length() > 0) {
+            if(!(Pattern.matches("[0-9]{6}", player.getPinCode()))){
+                return false;
+            }
+        }
+        if(Integer.parseInt(player.getCountry()) < 1) return false;
+        if(Integer.parseInt(player.getState()) < 1) return false;
+        if(Integer.parseInt(player.getCity()) < 1) return false;
+        System.out.println("validations done");
         return true;
     }
 }
