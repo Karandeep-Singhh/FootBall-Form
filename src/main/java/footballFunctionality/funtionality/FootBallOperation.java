6c42d564b637922292589f222a652e764a8cc48d
Karan <67946298+Karandeep-Singhh@users.noreply.github.com>
2022-02-15 16:59:42 +0530

    Update FootBallOperation.java

--- src/main/java/footballFunctionality/funtionality/FootBallOperation.java
+++ src/main/java/footballFunctionality/funtionality/FootBallOperation.java
@@ -4,7 +4,7 @@ import com.google.gson.Gson;
 //import footballFunctionality.FootBallDao;
 //import footballFunctionality.Player;
 //import footballFunctionality.Validation;
-import footballFunctionality.dao.FootBallDao;
+import footballFunctionality.dao.FootBallRegistrationDAO;
 import footballFunctionality.model.PlayerInfo;
 
 import javax.servlet.ServletInputStream;
@@ -16,103 +16,156 @@ import java.io.PrintWriter;
 
 public class FootBallOperation {
 
+    private static PrintWriter out = null;
+    private static Gson gson = null;
+    private static FootBallRegistrationDAO dbOperation = null;
+    private static String json = null;
+    private static String result = null;
 
-    public static void onPost(ServletInputStream inputStream, HttpServletResponse resp) throws IOException {
+    /**
+     * Performs operation upon POST requests
+     * @param inputStream : brings the inputstream data
+     * @param resp : usual response object
+     * @throws IOException
+     */
+    public static void onPost(ServletInputStream inputStream, HttpServletResponse resp) throws IOException
+    {
         BufferedReader br =
                 new BufferedReader(new InputStreamReader(inputStream));
-//        System.out.println("hitt");
-        String json = "";
-        if(br != null){
-            json = br.readLine();
-            System.out.println("helloServlet : "+json);
-        } else{
-            System.out.println("br is null");
-        }
-        PrintWriter out = resp.getWriter();
+
+        //--PrintWriter Object--
+        out = resp.getWriter();
         resp.setContentType("application/json");
         resp.setCharacterEncoding("UTF-8");
 
-        Gson gson = new Gson();
-        PlayerInfo player = gson.fromJson(json, PlayerInfo.class);
-//        System.out.println(player.getEmail());
-        FootBallDao register = new FootBallDao();
-        String result = "failed";
+        json = "";
 
-        if(Validation.isValid(player)) {
-            if (!register.checkUser(player)) {
-                result = register.insert(player);
+        if(br != null)
+        {
+            json = br.readLine();
+        }
+        else
+        {
+
+        }
+
+
+        gson = new Gson();
+        PlayerInfo player = gson.fromJson(json, PlayerInfo.class);
+
+        dbOperation = new FootBallRegistrationDAO();
+        result = "failed";
+
+        // if the fields data passes validations
+        if(Validation.isValid(player))
+        {
+            // Checks if user/player exists in database to ensure a new entry
+            if (dbOperation.checkUserExists(player))
+            {
+                result = "userexists";
+            }
+            result = dbOperation.registerNewPlayer(player);
+        }
+
+        player.setStatus(result);
+        out.print(new Gson().toJson(player));
+        out.flush();
+
+    }
+
+    /**
+     * Performs operation upon GET requests
+     * @param req
+     * @param resp
+     * @throws IOException
+     */
+    public static void onGet(String req, HttpServletResponse resp) throws IOException
+    {
+
+        json = req;
+
+        gson = new Gson();
+        PlayerInfo player =new PlayerInfo();
+        player.setUserName(json);
+
+        dbOperation = new FootBallRegistrationDAO();
+
+        //--PrintWriter Object--
+        out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        // Checks if user/player exists in database to retreive data
+        if(dbOperation.checkUserExists(player))
+        {
+            PlayerInfo registeredPlayer = dbOperation.retreivePlayer(player.getUserName());
+            registeredPlayer.setUserCheck(true);
+
+            result = new Gson().toJson(registeredPlayer);
+            System.out.println("This registered Player "+result);
+        }
+        else
+        {
+            player.setUserCheck(false);
+            result = new Gson().toJson(player);
+            System.out.println("this retreive servlet :" + result);
+        }
+
+        out.print(result);
+        out.flush();
+
+    }
+
+    /**
+     * Performs operation upon PUT requests
+     * @param inputStream
+     * @param resp
+     * @throws IOException
+     */
+    public static void onPut(ServletInputStream inputStream, HttpServletResponse resp) throws IOException
+    {
+        BufferedReader br =
+                new BufferedReader(new InputStreamReader(inputStream));
+
+        //--PrintWriter Object--
+        out = resp.getWriter();
+        resp.setContentType("application/json");
+        resp.setCharacterEncoding("UTF-8");
+
+        json = "";
+
+        if(br != null)
+        {
+            json = br.readLine();
+        }
+        else
+        {
+            out.print("failed");
+            return;
+        }
+
+        gson = new Gson();
+        PlayerInfo player = gson.fromJson(json, PlayerInfo.class);
+
+        dbOperation = new FootBallRegistrationDAO();
+
+        result = "failed";
+
+        if(Validation.isValid(player))
+        {
+            if (dbOperation.checkUserExists(player))
+            {
+                result = dbOperation.updateData(player);
                 System.out.println(result);
-            } else {
+            }
+            else
+            {
                 result = "userexists";
             }
         }
+
         player.setStatus(result);
-        String toBack = new Gson().toJson(player);
-        out.print(toBack);
-        out.flush();
-    }
-    public static void onGet(String req, HttpServletResponse resp) throws IOException {
-        String json = req;
-
-        Gson gson = new Gson();
-        PlayerInfo player =new PlayerInfo();
-        player.setUserName(json);
-        FootBallDao retreive = new FootBallDao();
-
-        PrintWriter out = resp.getWriter();
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-        if(retreive.checkUser(player)){
-            PlayerInfo registeredPlayer = retreive.retreivePlayer(player.getUserName());
-            registeredPlayer.setUserCheck(true);
-            String responsePlayer = new Gson().toJson(registeredPlayer);
-
-            System.out.println("This registered Player "+responsePlayer);
-
-            out.print(responsePlayer);
-            out.flush();
-        } else{
-            player.setUserCheck(false);
-            String userResponse = new Gson().toJson(player);
-            System.out.println("this retreive servlet :" + userResponse);
-            out.print(userResponse);
-            out.flush();
-        }
-    }
-    public static void onPut(ServletInputStream inputStream, HttpServletResponse resp) throws IOException {
-        BufferedReader br =
-                new BufferedReader(new InputStreamReader(inputStream));
-        String json = "";
-
-        if(br != null){
-            json = br.readLine();
-            System.out.println("update request : "+json);
-        } else{
-            System.out.println("br is null");
-        }
-
-        Gson gson = new Gson();
-        PlayerInfo player = gson.fromJson(json, PlayerInfo.class);
-
-        FootBallDao update = new FootBallDao();
-
-        PrintWriter out = resp.getWriter();
-        resp.setContentType("application/json");
-        resp.setCharacterEncoding("UTF-8");
-
-        String result = "failed";
-
-        if(Validation.isValid(player)) {
-            if (update.checkUser(player)) {
-                result = update.updateData(player);
-                System.out.println(result);
-            } else {
-                result = "User do not exist";
-            }
-        }
-        player.setStatus(result);
-        String toBack = new Gson().toJson(player);
-        out.print(toBack);
+        out.print(new Gson().toJson(player));
         out.flush();
     }
 }
